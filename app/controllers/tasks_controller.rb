114bba2fedeba47f6bcf34ba5403f2aef67057e6
negi <pengon13064@gmail.com>
2022-08-09 19:29:55 +0900

    新規作成画面を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,33 @@
 class TasksController < ApplicationController
-
+  before_action :set_task, only: %i[ show edit update ]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
   end
 
   def edit
-    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to task_url(@task), notice: "新規作成されました" }
+      else
+        format.html { render :new, status: :unprocessable_entity } #HTTP status 422
+      end
+    end
   end
 
   def update
-    @task = Task.find(params[:id])
-
     respond_to do |format|
       if @task.update(task_params)
         format.html { redirect_to task_url(@task), notice: "更新されました" }
@@ -25,6 +38,10 @@ class TasksController < ApplicationController
   end
 
   private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
     def task_params
       params.require(:task).permit(:name, :detail)
     end
