2ed11bd60c9d83c997cd34608dae99b601454932
negi <pengon13064@gmail.com>
2022-08-09 16:07:30 +0900

    詳細画面を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'tasks', controller: 'tasks', action: 'index'
-  get 'tasks/index'
+  resources :tasks, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
