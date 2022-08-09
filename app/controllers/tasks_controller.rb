336813ac5add25d30d1fb3fa217cb2d4e7a4a114
negi <pengon13064@gmail.com>
2022-08-09 19:08:59 +0900

    編集画面追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
@@ -6,4 +7,25 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to task_url(@task), notice: "更新されました" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:name, :detail)
+    end
 end
