c816a68f609a46c13ec9d7f02f76d2838c45a20c
negi <pengon13064@gmail.com>
2022-08-18 13:58:03 +0900

    タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,12 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # translate
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    # time zone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
