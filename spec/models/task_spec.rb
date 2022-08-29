9ed9493b423f2ab20c77790ef3105e6b9f23d7b7
negi <pengon13064@gmail.com>
2022-08-29 14:05:49 +0900

    テストを修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,7 +5,7 @@ context 'invalid parameter' do
     it 'error is displayed if name is blank.' do
       task = build(:task, name: nil)
       task.valid?
-      expect(task.errors[:name]).to include('タスク名を入力してください')
+      expect(task.errors[:name]).to include('を入力してください')
     end
   end
 end
