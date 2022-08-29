baf13f23a94607b3d983d326cd617f45decd5a4f
negi <pengon13064@gmail.com>
2022-08-29 19:02:57 +0900

    終了期限をViewに追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -50,6 +50,6 @@ class TasksController < ApplicationController
     end
 
     def task_params
-      params.require(:task).permit(:name, :detail)
+      params.require(:task).permit(:name, :detail, :deadline)
     end
 end
