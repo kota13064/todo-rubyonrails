82a075f90d895acbf39d1edab2568d0bbcb1138c
negi <pengon13064@gmail.com>
2022-09-05 12:14:43 +0900

    日本語に変更し、時間をTime.zoneにて設定

--- spec/requests/tasks_request_spec.rb
+++ spec/requests/tasks_request_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe TasksController, type: :request do
   describe '#index' do
-    it '200 OK' do
+    it 'リクエストがステータスコード200で成功すること' do
       get tasks_path
       expect(response).to have_http_status 200
     end
 
     context 'ソート条件なし' do
-      let!(:task1) { create(:task, created_at:'2022-08-23T12:00:00.000+09:00') }
-      let!(:task2) { create(:task, created_at:'2022-08-15T12:00:00.000+09:00') }
-      let!(:task3) { create(:task, created_at:'2022-08-22T12:00:00.000+09:00') }
+      let!(:task1) { create(:task, created_at: Time.zone.tomorrow) }
+      let!(:task2) { create(:task, created_at: Time.zone.yesterday) }
+      let!(:task3) { create(:task, created_at: Time.zone.today) }
       it '順序がタスクの作成日の降順となっていること' do
         get tasks_path
         expect(controller.instance_variable_get(:@tasks)).to eq [task1, task3, task2]
       end
     end
 
-    context '締め切り日でソート' do
-      let!(:task4) { create(:task, deadline:'2022-08-23T12:00:00.000+09:00') }
-      let!(:task5) { create(:task, deadline:'2022-08-15T12:00:00.000+09:00') }
-      let!(:task6) { create(:task, deadline:'2022-08-22T12:00:00.000+09:00') }
+    context 'ソート条件あり' do
+      let!(:task4) { create(:task, deadline:Time.zone.tomorrow) }
+      let!(:task5) { create(:task, deadline:Time.zone.yesterday) }
+      let!(:task6) { create(:task, deadline:Time.zone.today) }
       it '締め切り日の降順でソートが行われること' do
         get tasks_path, params: { order_column: 'deadline', order: 'asc' }
         expect(controller.instance_variable_get(:@tasks)).to eq [task5, task6, task4]
@@ -34,17 +34,17 @@ RSpec.describe TasksController, type: :request do
   end
 
   describe '#show' do
-    context 'When a task exists' do
+    context 'リクエストしたタスクが存在しているとき' do
       let!(:task) { create(:task) }
 
-      it '200 OK' do
+      it 'リクエストがステータスコード200で成功すること' do
         get task_path task
         expect(response).to have_http_status 200
       end
     end
 
-    context 'When a task does not exist' do
-      it '404 not found' do
+    context 'リクエストしたタスクが存在しないとき' do
+      it 'リクエストがステータスコード404で失敗すること' do
         get task_path 1
         expect(response).to have_http_status 404
       end
@@ -52,7 +52,7 @@ RSpec.describe TasksController, type: :request do
   end
 
   describe '#new' do
-    it '200 OK' do
+    it 'リクエストがステータスコード200で成功すること' do
       get new_task_path
       expect(response).to be_successful
       expect(response).to have_http_status 200
@@ -62,14 +62,14 @@ RSpec.describe TasksController, type: :request do
   describe '#edit' do
     let!(:task) { create(:task) }
 
-    it '200 OK' do
+    it 'リクエストがステータスコード200で成功すること' do
       get edit_task_path task
       expect(response).to have_http_status 200
     end
   end
 
   describe '#create' do
-    it '302 redirect' do
+    it 'リクエストがステータスコード302でリダイレクトすること' do
       post tasks_path, params: { task: attributes_for(:task) }
       expect(response).to have_http_status 302
       expect(response).to redirect_to Task.last
@@ -79,7 +79,7 @@ RSpec.describe TasksController, type: :request do
   describe '#update' do
     let!(:task) { create(:task) }
 
-    it '302 redirect' do
+    it 'リクエストがステータスコード302でリダイレクトすること' do
       put task_path task, params: { task: attributes_for(:task, name:'update name', detail:'update detail') }
       expect(response).to have_http_status 302
       expect(response).to redirect_to Task.last
@@ -89,7 +89,7 @@ RSpec.describe TasksController, type: :request do
   describe '#destroy' do
     let!(:task) { create(:task) }
 
-    it '302 redirect' do
+    it 'リクエストがステータスコード302でリダイレクトすること' do
       delete task_path task
       expect(response).to have_http_status 302
       expect(response).to redirect_to(tasks_path)
