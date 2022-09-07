463199fe8c66a98e01d390b066dfde3773a9b793
negi <pengon13064@gmail.com>
2022-09-07 14:34:09 +0900

    DBをresetしてmigrationをやり直した。

--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_083656) do
   create_table "tasks", force: :cascade do |t|
     t.text "name", null: false
     t.text "detail"
-    t.datetime "created_at", precision: nil, null: false
-    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.datetime "deadline"
     t.bigint "task_status_id"
     t.index ["task_status_id"], name: "index_tasks_on_task_status_id"
