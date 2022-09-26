6d5116371c4c49dbdbac2e82b7165ceedbeb49b4
negi <pengon13064@gmail.com>
2022-09-26 15:58:59 +0900

    rubocopにtags nameにユニークバリデーションを使用するならindexを貼れと言われた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_21_095445) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_26_065538) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_21_095445) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "task_statuses", force: :cascade do |t|
